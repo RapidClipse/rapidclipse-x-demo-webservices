9896b118039a3c4f432c72773d054e4b71cb031a
MS <m.stoer@xdev-software.de>
2019-11-04 08:39:36 +0100

    Fix webservices not working

--- src/main/java/com/rapidclipse/demo/webservices/AppServlet.java
+++ src/main/java/com/rapidclipse/demo/webservices/AppServlet.java
@@ -6,15 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import com.rapidclipse.framework.server.RapServlet;
 
 
-/**
- *
- */
 @WebServlet(urlPatterns = "/*", asyncSupported = true)
 public class AppServlet extends RapServlet
 {
-	/**
-	 *
-	 */
 	public AppServlet()
 	{
 		super();
